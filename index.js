3046f11843257a6b9f412534e1ab6a2a3d4078ba
Rodrigo Marros <rodrigo.do.marros@gmail.com>
2021-09-21 17:23:35 -0300

    parte 1

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const reddit = require('./reddit.js');
 (async () => {
 
     await reddit.initialize('node');
-
+    let results = await reddit.getResults(10);
+    
 })();
--- reddit.js
+++ reddit.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const SUBREDDIT_URL = (reddit) => `https://old.reddit.com/user/${reddit}/`;
+const SUBREDDIT_URL = (reddit) => `https://old.reddit.com/r/${reddit}/`;
 
 
 const self = {
@@ -14,9 +14,33 @@ const self = {
 
         /*go to subreddit*/
         await self.page.goto(SUBREDDIT_URL(reddit), {waitUntil: 'networkidle0' });
+      
+    },
 
-        
+    getResults: async (nr) => {
+        let elements = await self.page.$$('#siteTable > div[class*="thing"]');
+        let results = [];
+        for(let element of elements){
 
+            let title = await element.$eval(('p[class="title"]'), node => node.innerText.trim());
+            let rank = await element.$eval(('p[class="rank"]'), node => node.innerText.trim());
+            let postTime = await element.$eval(('p[class="tagline"] > time'), node => node.getAttribute('title'));
+            let authorUrl = await element.$eval(('p[class="tagline"] > a[class*="author"]'), node => node.getAttribute('href'));
+            let authorName = await element.$eval(('p[class="tagline"] > a[class*="author"]'), node => node.innerText.trim());
+            let score = await element.$eval(('div[class="score likes"]'), node => node.innerText.trim());
+            let comments = await element.$eval(('a[data-event-action="comments"]'), node => node.innerText.trim());
+
+            results.push({
+                title,
+                rank,
+                postTime,
+                authorUrl,
+                authorName,
+                score,
+                comments
+            })
+        }
+        return results;
     }
 
 }
